b79550c8a7a9c7738bd28077dd602b4bd202aff2
Ryoma Morita <p1ha1czx@s.okayama-u.ac.jp>
2026-07-07 15:30:13 +0900

    Dockerイメージのビルド方法を修正し、環境変数のチェックを削除してSingularity.defを使用するように変更

--- job.sh
+++ job.sh
@@ -6,17 +6,7 @@
 #PJM -L jobenv=singularity
 #PJM -j
 
-if [ -z $SINGULARITY_DOCKER_USERNAME ]; then
-    echo "UNDEFINED SINGULARITY_DOCKER_USERNAME"
-    exit 1
-fi
-
-if [ -z $SINGULARITY_DOCKER_PASSWORD ]; then
-    echo "UNDEFINED SINGULARITY_DOCKERPASSWORD"
-    exit 2
-fi
-
 module load singularity-ce
 
-singularity build fluid_analysis_util.sif docker://ghcr.io/$SINGULARITY_DOCKER_USERNAME/fluid_analysis_util:latest
+singularity build fluid_analysis_util.sif Singularity.def
 singularity run fluid_analysis_util.sif

ac183c87e972eca1eec38cd54d68b4c679acbf13
Ryoma Morita <p1ha1czx@s.okayama-u.ac.jp>
2026-07-21 13:40:27 +0900

    冗長なデータローダーの初期化処理を削除し、ファイルパターンを直接使用してデータを取得するように変更。グリッド計算を簡素化し、結果をCSVファイルに書き出す処理を改善。

--- coarsen_grid.py
+++ coarsen_grid.py
@@ -1,17 +1,8 @@
+import glob
 import json
-import numpy as np
 import polars as pl
+from polars import col
 
-from sim_data_loader import SimulationDataLoader
-
-def init_loader(dataset: str) -> SimulationDataLoader:
-    base_path = f"/data/arrow_files/{dataset['data_type']}_{dataset['data_size']}_{dataset['step']}"
-
-    data_pattern = f"{base_path}/data/{dataset['data_type']}_{dataset['data_size']}_*.arrow"
-    
-    index_file = f"{dataset['data_type']}_{dataset['data_size']}_index.json"
-
-    return SimulationDataLoader(data_pattern, index_file)
 
 def main():
     dataset_key = "keta2_4096"
@@ -21,39 +12,20 @@ def main():
     
     dataset = options[dataset_key]
 
-    loader = init_loader(dataset)
-
-    size = dataset['data_size']
-
     coarsen_size = 64
 
-    num_blocks = size // coarsen_size
+    base_path = f"/data/arrow_files/{dataset['data_type']}_{dataset['data_size']}_{dataset['step']}"
+    data_pattern = f"{base_path}/data/{dataset['data_type']}_{dataset['data_size']}_*.arrow"
+    files = glob.glob(data_pattern)
 
-    block_coords = np.linspace(1, size, num_blocks)
+    df = pl.scan_ipc(files)
 
-    I, J, K = np.meshgrid(block_coords, block_coords, block_coords, indexing='ij')
-    I = np.reshape(I, (-1,))
-    J = np.reshape(J, (-1,))
-    K = np.reshape(K, (-1,))
+    expr_i = ((col("gnx") - 1) // coarsen_size + 1).alias("i")
+    expr_j = ((col("gny") - 1) // coarsen_size + 1).alias("j")
+    expr_k = ((col("nn") - 1) // coarsen_size + 1).alias("k")
 
-    eps = np.zeros_like(I, dtype=float)
-    columns = ["gnx", "gny", "nn", "u", "v", "w", "eps"]
-
-    for idx, (i, j, k) in enumerate(zip(I, J, K)):
-        gnx = (i - 1) * coarsen_size + 1
-        gny = (j - 1) * coarsen_size + 1
-        nn = (k - 1) * coarsen_size + 1
-
-        df_fetched = loader.fetch_boxcell(gnx, gny, nn, coarsen_size, columns, streaming=True)
-
-        df_eps = df_fetched.select("eps")
-        eps[idx] = df_eps.mean().item()
-
-    data = np.vstack([I, J, K, eps]).T
-
-    df = pl.LazyFrame(data, schema=["i", "j", "k", "eps"])
-
-    df.collect().write_csv("coarsened_grid.csv")
+    df = df.with_columns([expr_i, expr_j, expr_k]).group_by(["i", "j", "k"]).agg(col("eps").mean()).sort(["i", "j", "k"])
+    df = df.collect().write_csv("coarsened_data.csv")
 
 if __name__ == "__main__":
     main()

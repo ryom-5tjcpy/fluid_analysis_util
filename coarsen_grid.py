445b8992242a677c6fe4b46a8ec61cd314cd0d73
Ryoma Morita <p1ha1czx@s.okayama-u.ac.jp>
2026-07-23 10:19:17 +0900

    処理の実行時間を計測するためのタイマーを追加し、データフレームの集約処理を改善

--- coarsen_grid.py
+++ coarsen_grid.py
@@ -18,6 +18,9 @@ def main():
     data_pattern = f"{base_path}/data/{dataset['data_type']}_{dataset['data_size']}_*.arrow"
     files = glob.glob(data_pattern)
 
+    import time
+    start = time.perf_counter()
+
     df = pl.scan_ipc(files)
 
     expr_i = ((col("gnx") - 1) // coarsen_size + 1).alias("i")
@@ -25,8 +28,11 @@ def main():
     expr_k = ((col("nn") - 1) // coarsen_size + 1).alias("k")
 
     df = df.with_columns([expr_i, expr_j, expr_k])
-    df = df.group_by(["i", "j", "k"]).agg(col("eps").mean()).sort(["i", "j", "k"])
-    df.collect().write_csv("coarsened_data.csv")
+    df = df.group_by(["i", "j", "k"]).agg(col("eps").count().alias("eps_count"), col("eps").mean().alias("eps_mean"), col("eps").std().alias("eps_std"), col("eps").sum().alias("eps_sum"), col("eps").min().alias("eps_min"), col("eps").max().alias("eps_max"), col("eps").median().alias("eps_median")).sort(["i", "j", "k"])
+    df.collect(engine='streaming').write_csv("coarsened_data.csv")
+
+    elapse = time.perf_counter() - start
+    print(f"Elapsed time: {elapse}")
 
 if __name__ == "__main__":
     main()
